98df71e12b8686f0edba368be7416ba93d91a118
teatonic <teatonic@gmail.com>
2020-11-20 22:35:30 +0100

    init version

---
+++ README.md
@@ -0,0 +1,1 @@
+nohup ./speedtest.sh &
---
+++ speedtest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+LOGFILE="speedtest_`date +%Y_"%m_%d_%H%m%S"`.log"
+echo "---------------- Start-------------- " > $LOGFILE 
+
+while true
+do
+        TIMESTP=`date +%Y-"%m-%d %H:%M:%S"`
+        SPEED=`fast`
+        echo "$SPEED;$TIMESTP" >> $LOGFILE 
+	sleep 30
+done
+
+echo "----------End-------------  " > $LOGFILE 
+
