775c082a3a6d63cc412481ce3e19072024d5241b
teatonic <teatonic@gmail.com>
2020-12-04 17:00:59 +0100

    to csv

--- speedtest.sh
+++ speedtest.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+RES_FILE=speedtest.csv
 LOGFILE="speedtest_`date +%Y_"%m_%d_%H%m%S"`.log"
-echo "---------------- Start-------------- " > $LOGFILE 
+#echo "---------------- Start-------------- " > $LOGFILE 
 
-while true
+LOCK_FILE=speedtest.lck
+touch $LOCK_FILE
+while  [ -f "$LOCK_FILE" ]
 do
-        TIMESTP=`date +%Y-"%m-%d %H:%M:%S"`
+        TIMESTP=`date +%Y-"%m-%d-%H:%M:%S"`
         SPEED=`fast`
-        echo "$SPEED;$TIMESTP" >> $LOGFILE 
+        SPEEDO=$(sed 's/ -> //g' <<< $SPEED | sed 's/ Mbps//g')
+        echo "$SPEEDO;$TIMESTP" >> $RES_FILE 
 	sleep $1
 done
 
-echo "----------End-------------  " >> $LOGFILE 
+#echo "----------End-------------  " >> $LOGFILE 
 
